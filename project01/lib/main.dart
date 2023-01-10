5b02ed0fb4845a8f8b83f90e0b1b5105f9d76e72
Azizur <106616550+Mitonaziz@users.noreply.github.com>
2023-01-10 11:59:26 +0600

    Make app and add image

--- project01/lib/main.dart
+++ project01/lib/main.dart
@@ -10,8 +10,10 @@ void main() => runApp(
             title: Text('I am Azizur'),
             backgroundColor: Colors.pinkAccent,
         ),
-          body: Image(
-            image: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3Wjd-Cak78mooEfHTx64D7xhFtgBMZRFiiQ&usqp=CAU'),
+          body: Center(
+            child: Image(
+              image: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3Wjd-Cak78mooEfHTx64D7xhFtgBMZRFiiQ&usqp=CAU'),
+            ),
           ) ,
         )
       ),
